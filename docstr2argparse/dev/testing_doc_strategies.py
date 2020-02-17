9d5655e38a59f5f19a5ca262b906958cfc23ae8c
matteo <matteo.lacki@gmail.com>
2020-02-17 18:09:51 +0100

    loosing ones mind

--- docstr2argparse/dev/testing_doc_strategies.py
+++ docstr2argparse/dev/testing_doc_strategies.py
@@ -8,16 +8,15 @@ from vodkas.plgs import parse_plgs_doc
 
 from docstr2argparse.parse import parse_google, get_positional_or_keyword_params, foo2argparse, document
 
-
 def foo():
     pass
+
 parse_google(foo.__doc__)
 parse_google(apex3d.__doc__)
 parse_google(get_fastas.__doc__)
 f = apex3d
 
-foo2argparse(f)
-
+foo2argparse(plgs)[1]
 document(apex3d).print_help()
 document(peptide3d).print_help()
 document(iadbs).print_help()
@@ -31,6 +30,8 @@ plgs_desc, parsed, arg2foo = parse_plgs_doc()
 # ap = argparse.ArgumentParser(description='Analyze Waters Raw Data with PLGS.')
 # ap.print_help()
 from collections import defaultdict
+
+
 
 
 
