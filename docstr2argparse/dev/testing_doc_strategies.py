f2d51ab0594e837c8a89d920cbc691c8a59c66ca
matteo <matteo.lacki@gmail.com>
2020-02-25 11:43:23 +0100

    small changes in tests

--- docstr2argparse/dev/testing_doc_strategies.py
+++ docstr2argparse/dev/testing_doc_strategies.py
@@ -2,36 +2,14 @@
 %autoreload 2
 import argparse
 import re
+from pathlib import Path
 
-from vodkas import apex3d, peptide3d, iadbs, plgs, get_fastas
-from vodkas.plgs import parse_plgs_doc
-
+from vodkas import apex3d, peptide3d, iadbs, plgs
 from docstr2argparse.parse import parse_google, get_positional_or_keyword_params, foo2argparse, document
+from vodkas.fastas import get_fastas
 
-def foo():
-    pass
-
-parse_google(foo.__doc__)
-parse_google(apex3d.__doc__)
-parse_google(get_fastas.__doc__)
-f = apex3d
-
-foo2argparse(plgs)[1]
-document(apex3d).print_help()
-document(peptide3d).print_help()
-document(iadbs).print_help()
-
-args = document_many({'ape': apex3d, 'ia':iadbs, 'pep':peptide3d})
-args.print_help()
-
-from pprint import pprint
-pprint(foo2argparse(apex3d))
-plgs_desc, parsed, arg2foo = parse_plgs_doc()
-# ap = argparse.ArgumentParser(description='Analyze Waters Raw Data with PLGS.')
-# ap.print_help()
-from collections import defaultdict
-
-
-
-
+p = Path(r"X:\SYMPHONY_VODKAS\fastas\custom\Ute_fucking_bigger_fastas\20200124_upsp_human_yeas8_ecoli_cont_rev_validated.fasta")
+# p = Path("/mnt/ms/data/SYMPHONY_VODKAS/fastas/custom/Ute_fucking_bigger_fastas/20200124_upsp_human_yeas8_ecoli_cont_rev_validated.fasta")
+p.exists()
+get_fastas(p)
 
